b8a453a7905e1efa2d7d2e08b26d855404160e7b
Ela04 <ga.antunez@duocuc.cl>
2023-04-20 21:11:14 -0400

    consola

--- Valid_Form/js/app.js
+++ Valid_Form/js/app.js
@@ -1,1 +1,17 @@
-console.log("Estamos Conectados")
+console.log("Estamos Conectados");
+document.write("<h1> ALERTA DESDE JS </h1>");
+/*alert("!!!! DANGER ¡¡¡¡")*/
+
+var nt1 = 4.5;
+var nt2 = 2.0;
+var nt3 = 7.0;
+var sum = (nt1+nt2+nt3);
+prom = sum/3;
+console.log("La suma de sus notas es: ",sum);
+//console.log("El promedio de sus notas es: ",sum/3);
+if(prom > 4.0){
+    console.log("Messirve, lo demas es lujo");
+}
+else{
+    console.log("Casi casi");
+}
