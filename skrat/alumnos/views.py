6c7f4219b48d939875e53c0900bcc5d60924f1e4
Skrat23 <ben.escarate@duocuc.cl>
2023-06-08 21:00:13 -0400

    atualizado

--- skrat/alumnos/views.py
+++ skrat/alumnos/views.py
@@ -1,8 +1,15 @@
 
 
 # Create your views here.
-from django.shortcuts import render
+#from django.shortcuts import render
+#from django.http import HttpResponse
+
+#def alumnos(request):
+ #   return HttpResponse("Hello world!")
+
 from django.http import HttpResponse
+from django.template import loader
 
 def alumnos(request):
-    return HttpResponse("Hello world!")
+  template = loader.get_template('1.html')
+  return HttpResponse(template.render())
