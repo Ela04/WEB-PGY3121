8a9217a01085c377044405457e808f9c7c925194
Ela04 <ga.antunez@duocuc.cl>
2023-06-15 17:45:27 -0500

    b

--- skrat/alumnos/urls.py
+++ skrat/alumnos/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('alumnos/', views.alumnos, name='alumnos'),
+    #path('alumnos/', views.alumnos, name='alumnos'),
+    path('', views.home, name='home'),
+    path('Base', views.Base, name='Base'),
+    path('Carrucel', views.Carrucel, name='Carrucel'),
+    path('Nosotros', views.Nosotros, name='Nosotros'),
+    path('Noticias', views.Noticias, name='Noticias'),
+    path('Registrate', views.Registrate, name='Registrate'),
 ]
